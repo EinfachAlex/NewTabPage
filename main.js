599bc82b7358744647e5aefeaa7821d63ebc7a93
EinfachAlex <33007348+EinfachAlex@users.noreply.github.com>
2019-01-25 18:10:31 +0100

    dynamic background

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+var settings;
+
+function loadSettings(callback) {
+    var settingsFile = new XMLHttpRequest();
+    settingsFile.overrideMimeType("application/json");
+    settingsFile.open("GET", "./settings.json", true);
+    settingsFile.onreadystatechange = function () {
+        if (settingsFile.readyState === 4 && settingsFile.status == "200") {
+            settings = JSON.parse(settingsFile.responseText);
+            callback();
+        }
+    }
+    settingsFile.send(null);
+}
+
 function updateCryptoTicker() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
@@ -21,15 +36,95 @@ function updateCryptoTicker() {
     xhttp.send();
 }
 
-function updateClock(){
+function updateClock() {
     var time = new Date();
 
     document.getElementsByClassName('clock')[0].innerHTML = time.getHours() + " " + time.getMinutes();
-
-    if (time.getMinutes() < 10){
+    if (time.getMinutes() < 10) {
         document.getElementsByClassName('clock')[0].innerHTML = time.getHours() + " 0" + time.getMinutes();
     }
 }
 
-updateCryptoTicker();
-updateClock();
+function updateBackground() {
+    var date = new Date();
+    var daytime = getDaytime(date.getHours());
+    var picture = getPicture(daytime, getNextTime(daytime));
+    var path;
+
+    if (picture < 10) {
+        path = "'./assets/pictures/" + daytime + "_00000" + picture + ".jpeg'";
+    } else if (picture < 100) {
+        path = "'./assets/pictures/" + daytime + "_0000" + picture + ".jpeg'";
+    } else if (picture < 1000) {
+        path = "'./assets/pictures/" + daytime + "_000" + picture + ".jpeg'";
+    }
+    console.log(path);
+
+    document.body.style.backgroundImage = "url(" + path + ")";
+}
+
+function getDaytime(hour) {
+    console.log(hour);
+    if (hour < settings.background.sunrise) {
+        return 'night';
+    }
+    if (hour >= settings.background.sunrise && hour < settings.background.morning) {
+        return 'sunrise';
+    }
+    if (hour >= settings.background.morning && hour < settings.background.afternoon) {
+        return 'morning';
+    }
+    if (hour >= settings.background.afternoon && hour < settings.background.sunset) {
+        return 'afternoon';
+    }
+    if (hour >= settings.background.sunset && hour < settings.background.evening) {
+        return 'sunset';
+    }
+    if (hour >= settings.background.evening) {
+        return 'evening';
+    }
+}
+
+function getPicture(daytime, nextTime) {
+    var partStartDate = new Date();
+    partStartDate.setHours(settings.background[daytime], 0, 0, 0);
+    var partLength = (settings.background[nextTime] - settings.background[daytime]);
+    var partTimePassed = (Date.now() - partStartDate.getTime()) / 1000;
+    var partPictures = settings.background.pictures[daytime];
+    var partPercentPassed = (partTimePassed / 60 / 60) / partLength * 100;
+    var pictureNumber = partPercentPassed / 100 * partPictures;
+
+    return pictureNumber.toFixed(0);
+}
+
+function getNextTime(daytime) {
+    switch (daytime) {
+        case 'night':
+            return 'sunrise';
+        case 'sunrise':
+            return 'morning';
+        case 'morning':
+            return 'afternoon';
+        case 'afternoon':
+            return 'sunset';
+        case 'sunset':
+            return 'evening';
+        case 'evening':
+            return 'night1';
+    }
+}
+
+function startRefreshing() {
+    updateClock();
+    updateCryptoTicker();
+    updateBackground();
+    setInterval(function () {
+        updateClock();
+        updateCryptoTicker();
+        updateBackground();
+    }, 30000);
+}
+
+loadSettings(startRefreshing);
+
+//localStorage["inputText"] = data;
